05036c6d42d74d2b44beb92be1c7f59475d862a9
vasscisse <vass.cisse@gmail.com>
2018-12-25 22:50:12 +0000

    Updates

--- precache-manifest.924111c423e4c0ed981313f3f8395250.js
+++ precache-manifest.924111c423e4c0ed981313f3f8395250.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-samegame/static/js/runtime~main.04f4866f.js"
   },
   {
-    "revision": "383b279a21af48597dc0",
-    "url": "/react-samegame/static/js/main.383b279a.chunk.js"
+    "revision": "8320fe5011eccfadc5dd",
+    "url": "/react-samegame/static/js/main.8320fe50.chunk.js"
   },
   {
     "revision": "4f29bdfac74402aecb4c",
     "url": "/react-samegame/static/js/1.4f29bdfa.chunk.js"
   },
   {
-    "revision": "383b279a21af48597dc0",
+    "revision": "8320fe5011eccfadc5dd",
     "url": "/react-samegame/static/css/main.f00008b0.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-samegame/static/css/1.a347712c.chunk.css"
   },
   {
-    "revision": "c51698c73fc563215c89bdf24ba183ca",
+    "revision": "c00f502c00372087fe7375721f98418e",
     "url": "/react-samegame/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-samegame/precache-manifest.d4eb3eaf786a0f624e168e67c748abd5.js"
+  "/react-samegame/precache-manifest.924111c423e4c0ed981313f3f8395250.js"
 );
 
 workbox.clientsClaim();
