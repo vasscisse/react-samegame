b3d42be0640984efd6397d1adb9e74129f59f27f
vasscisse <vass.cisse@gmail.com>
2018-12-25 21:46:39 +0000

    Updates

---
+++ precache-manifest.45ce76dfb28e352be02b9b2a6157a361.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67d93ad90c1eb6a0241f",
+    "url": "/vasscisse/react-samegame/static/js/runtime~main.67d93ad9.js"
+  },
+  {
+    "revision": "7751daeef3af09649989",
+    "url": "/vasscisse/react-samegame/static/js/main.7751daee.chunk.js"
+  },
+  {
+    "revision": "4f29bdfac74402aecb4c",
+    "url": "/vasscisse/react-samegame/static/js/1.4f29bdfa.chunk.js"
+  },
+  {
+    "revision": "7751daeef3af09649989",
+    "url": "/vasscisse/react-samegame/static/css/main.f00008b0.chunk.css"
+  },
+  {
+    "revision": "4f29bdfac74402aecb4c",
+    "url": "/vasscisse/react-samegame/static/css/1.a347712c.chunk.css"
+  },
+  {
+    "revision": "d7c344742e7b86d90cfb1cc5e89513e2",
+    "url": "/vasscisse/react-samegame/index.html"
+  }
+];
